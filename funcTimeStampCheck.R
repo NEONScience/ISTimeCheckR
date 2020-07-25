3f155eb0350440511a3f5a6c62a9d5e07d887479
Kevin <kevinstyersgmu@gmail.com>
2020-07-24 18:11:53 -0600

    Update funcTimeStampCheck.R

--- funcTimeStampCheck.R
+++ funcTimeStampCheck.R
@@ -13,15 +13,17 @@ ping <- function(x, stderr = FALSE, stdout = FALSE, ...){
   if (pingvec == 0) TRUE else FALSE
 }
 
-ip = "10.116.17.2"
-password = "resuresu"
-pullTime = "2"
-path = "c:/GitHub/ISTimeCheckR/reports/TIS/"
+# ip = "10.116.17.2"
+# password = "resuresu"
+# pullTime = "2"
+# path = "c:/GitHub/ISTimeCheckR/reports/TIS/"
 
 TimeStampCheck <- function(ip, password, pullTime, path) {
   
   # Check connection
+  message(paste0("Pinging ", ip))
   pingResult <- ping(ip)
+  message(paste0("Ping Result was... ", pingResult))
       if(pingResult == TRUE){
       ## Initialize Connection
       session <- ssh::ssh_connect(host = paste0("user@",ip),passwd = password)
@@ -76,32 +78,15 @@ TimeStampCheck <- function(ip, password, pullTime, path) {
       ftest <- f %>%
         distinct(Eeprom,.keep_all = TRUE) %>%
         mutate(diffTime = difftime(EndTime,TimeStamp)) %>%
+        mutate(diffTimeStart = difftime(StartTime,TimeStamp)) %>%
         mutate(siteID = siteIDz)
       
-      write.csv(ftest, paste0(path,siteIDz,Sys.Date(),"special2.csv"), row.names = FALSE)
+      write.csv(ftest, paste0(path,siteIDz,Sys.Date(),"diff_time.csv"), row.names = FALSE)
       } else {
         message(paste0("No data pulled from ", ip))
       }
       } else {
-        noIpRaw <- fread("C:/GitHub/ISTimeCheckR/reports/noData/noData.csv",stringsAsFactors = FALSE)
-        noIp <- ""
-        noIp$date <- as.character(Sys.Date()) 
-        noIp$ip <- ip
-        noIp <- as.data.frame(noIp)
-        noIp$X.. <- as.logical(noIp$X..)
-        # str(noIp)
-        # str(noIpRaw)
-        ipList <- list(noIpRaw,noIp)
-        masterIP <- rbindlist(ipList)
-        write.csv(masterIP, "C:/GitHub/ISTimeCheckR/reports/noData/noData.csv",row.names = F,)
         message(paste0("Could not connect to ", ip))
-
-  # Save histogram
-  # ggplot2::ggplot(data=ftest,aes(x=diffTime))+
-  #   ggplot2::geom_histogram(bins = 10)+
-  #   ggplot2::labs(x = "Difference between Time final and LC time (seconds)", "Number of Streams", title = paste0(siteIDz, "'s Time Difference Analysis "))
-  # 
-  # ggsave(filename = paste0("C:/GitHub/TimeCheck/reports/",siteIDz,"Histogram.png"), device = "png")
       }
 }
 
@@ -111,9 +96,9 @@ metaData <- fread("C:/GitHub/ISTimeCheckR/metadata.csv")
 for(i in metaData$ipAddress[1:47]){
   TimeStampCheck(ip = i, password = "resuresu",pullTime = "2", path = "c:/GitHub/ISTimeCheckR/reports/TIS/")
 }
-for(i in metaData$ipAddress[77:77]){
-  TimeStampCheck(ip = i, password = "resuresu",pullTime = "2", path = "c:/GitHub/ISTimeCheckR/reports/TIS/")
-}
+# for(i in metaData$ipAddress[77:77]){
+#   TimeStampCheck(ip = i, password = "resuresu",pullTime = "2", path = "c:/GitHub/ISTimeCheckR/reports/TIS/")
+# }
 
 ### Stack all the resulting data with the code below if you so chose
 
@@ -132,32 +117,47 @@ files$StartTime <- as.POSIXct(files$StartTime, tz="UTC")
 files$EndTime <- as.POSIXct(files$EndTime, tz="UTC")
 files$diffTime <- round(files$diffTime, 2)
 files$`Sensor's Timestamp Difference` <- files$diffTime
+# files <- files %>%
+#   dplyr::group_by
+#   dplyr::mutate(diffTime2 = difftime(EndTime, StartTime))
 
 
 
 timeData2 <- files %>%
-  group_by(siteID) %>%
+  group_by(siteID, PullDate) %>%
   mutate(`Site's Median Timestamp Difference` = median(`Sensor's Timestamp Difference` )) %>%
-  mutate(`Sensor's Deviation from the Site's Median Timestamp` = abs(`Site's Median Timestamp Difference`-`Sensor's Timestamp Difference` )) %>%
-  filter(`Sensor's Deviation from the Site's Median Timestamp` > 5) %>%
-  filter(PullDate >= Sys.Date()-14)
+  mutate(`Sensor's Deviation from the Site's Median Timestamp` = abs(`Site's Median Timestamp Difference`-`Sensor's Timestamp Difference` )) 
 
+## Changing data a tiny bit just to get it to the Server with out updateing that code agggain.
 
-analysisPlot <- ggplot2::ggplot(data=timeData2,aes(x=`Sensor's Deviation from the Site's Median Timestamp`,
-                                                   SensorMac=MacAddress,fill=siteID,
-                                                   SiteMedianDifferenceTime = `Site's Median Timestamp Difference`))+
-  ggplot2::geom_histogram(bins = 30, binwidth = 5)+
-  ggplot2::labs(x = "Difference between Time final and LC time (seconds)", 
-                y = "Count of Streams in Bin",
-                "Number of Streams", 
-                title = paste0("Time Difference Histogram Analysis"))
-analysisPlot
-ggplotly(analysisPlot)
-str(timeData)
+timeData3 <- timeData2 %>%
+  dplyr::mutate(diffTime = diffTimeStart) %>%
+  dplyr::select(-diffTimeStart) #%>%
+  # dplyr::filter(`Sensor's Deviation from the Site's Median Timestamp` > 5)
 
-# Upload files to dropbox
+totalSitesForToday <- timeData3 %>%
+  filter(PullDate == Sys.Date())
+message(length(unique(totalSitesForToday$siteID)))
+
+# Upload files to dropbox for Swift
 library(rdrop2)
 local_KS_token_Path <- "C:/GitHub/swift/SwiftShinyDash/data/token.RDS"
 drop_auth(rdstoken = local_KS_token_Path)
-saveRDS(timeData2,"C:/Users/kstyers/Dropbox/SwiftData/swiftTimeCheck.rds")
-drop_upload(      "C:/Users/kstyers/Dropbox/SwiftData/swiftTimeCheck.rds","SwiftData")
+saveRDS(timeData3,"C:/Users/kstyers/Dropbox/SwiftData/swiftTimeCheck.rds")
+drop_upload("C:/Users/kstyers/Dropbox/SwiftData/swiftTimeCheck.rds","SwiftData")
+
+
+# Upload files to dropbox for Sensor Health
+library(rdrop2)
+local_KS_token_Path <- "C:/GitHub/swift/SwiftShinyDash/data/token.RDS"
+drop_auth(rdstoken = local_KS_token_Path)
+
+timeData4 <- timeData3 %>%
+  dplyr::select(siteID, PullDate, MacAddress, Eeprom, TimeStamp, EndTime, `Sensor's Deviation from the Site's Median Timestamp`)
+
+saveRDS(timeData4,"C:/Users/kstyers/Dropbox/SwiftData/sensor.health.TimeCheck.rds")
+drop_upload("C:/Users/kstyers/Dropbox/SwiftData/sensor.health.TimeCheck.rds","SwiftData")
+
+TodaySdata <- timeData4 %>%
+  dplyr::filter(PullDate == Sys.Date())
+View(TodaySdata)
